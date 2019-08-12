254e6347fba02b624adcf57a255a7819462a132c
Ce Gao <gaoce@caicloud.io>
2019-08-12 10:37:24 +0800

    fix: Avoid removing without warnings
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- scripts/install-native-operator.sh
+++ scripts/install-native-operator.sh
@@ -7,7 +7,13 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
 cd $SCRIPT_ROOT
-mkdir -p $GOPATH/src/github.com/caicloud
-rm -rf $GOPATH/src/k8s.io/sample-controller
-cp -r $(pwd)/native-demo-operator $GOPATH/src/k8s.io/sample-controller
+mkdir -p ${GOPATH}/src/k8s.io
+if [ -d "${GOPATH}/src/k8s.io/sample-controller" ] 
+then
+    echo "${GOPATH}/src/k8s.io/sample-controller exists. Please delete the directory after backup."
+    exit 1
+else
+    cp -r $(pwd)/native-demo-operator $GOPATH/src/k8s.io/sample-controller
+    echo "The lab is installed in ${GOPATH}/src/k8s.io/sample-controller, good luck!"
+fi
 cd - > /dev/null
